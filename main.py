a9ed7f3deb9e33cc3e5159c7dc670cd7ca4c113d
Elvis Eli <segbawuel@aiti-kace.com>
2021-03-02 10:13:00 +0000

    basedir comments

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ api.mount('/images', StaticFiles(directory=f"{image_DIR}"), name='images')
 api.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 from views import *
+
+# from pathlib import Path
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
